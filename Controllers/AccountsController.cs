a853632481682b1c5d412a8165a904a460c45a48
Masanaser <masadawood29@gmail.com>
2024-09-05 17:37:53 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,67 @@
+﻿
+
+
+
+using Identity.Data;
+using Identity.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public readonly ApplicationDbContext context;
+        public readonly UserManager<IdentityUser> userManager;
+        public readonly SignInManager<IdentityUser> signInManager;
+
+
+        public AccountsController(ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.signInManager = signInManager;
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return View(model);
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+
+            }
+            return View(model);
+
+        }
+    }
+}
